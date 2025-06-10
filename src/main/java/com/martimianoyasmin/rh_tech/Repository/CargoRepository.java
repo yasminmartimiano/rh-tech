d151b46e029e845ea9d6470246781df04638ee8b
yasminmartimiano <yasmin52@gmail.com>
2025-06-10 20:03:35 -0300

    feat: adicionou anotação que faltava

--- src/main/java/com/martimianoyasmin/rh_tech/Repository/CargoRepository.java
+++ src/main/java/com/martimianoyasmin/rh_tech/Repository/CargoRepository.java
@@ -1,7 +1,9 @@
 package com.martimianoyasmin.rh_tech.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.martimianoyasmin.rh_tech.Model.CargoModel;
 
+@Repository
 public interface CargoRepository extends JpaRepository<CargoModel, Long>{
 }
