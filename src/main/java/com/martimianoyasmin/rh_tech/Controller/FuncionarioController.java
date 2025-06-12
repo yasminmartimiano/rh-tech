62240aa0b75f9683f27040da6c001af77209553d
yasminmartimiano <yasminliver52@gmail.com>
2025-06-12 13:10:28 -0300

    feat: FuncionarioService e FuncionarioController finalizados.

--- src/main/java/com/martimianoyasmin/rh_tech/Controller/FuncionarioController.java
+++ src/main/java/com/martimianoyasmin/rh_tech/Controller/FuncionarioController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,33 +18,38 @@ import com.martimianoyasmin.rh_tech.Model.FuncionarioModel;
 import com.martimianoyasmin.rh_tech.Service.FuncionarioService;
 
 
+
 @RestController
 @RequestMapping("/funcionario")
 public class FuncionarioController {
 
     @Autowired
-    private FuncionarioService service;
+    private FuncionarioService funcionarioService;
 
     public List<FuncionarioModel> listarFuncionario(){
-        return service.listarFuncionario();
+        return funcionarioService.listarFuncionario();
     }
 
+    @PutMapping("/{id}")
+    public FuncionarioModel atualizarFuncionario(@PathVariable Long id, @RequestBody FuncionarioModel funcionarioModel) {
+        return funcionarioService.atualizarFuncionario(id, funcionarioModel);
+    }
 
     @PostMapping
     public FuncionarioModel salvarFuncionario(@RequestBody FuncionarioModel funcionarioModel) {
-        return service.salvarFuncionario(funcionarioModel);
+        return funcionarioService.salvarFuncionario(funcionarioModel);
     }
     
     @GetMapping("/{id}")
     public ResponseEntity<FuncionarioModel> buscarPorId(@RequestParam Long id) {
-        return service.buscarPorId(id)
+        return funcionarioService.buscarPorId(id)
                       .map(ResponseEntity::ok)
                       .orElse(ResponseEntity.notFound().build());
     }
     
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id){
-        service.deletar(id);
+        funcionarioService.deletar(id);
         return ResponseEntity.noContent().build();
     }
 }
